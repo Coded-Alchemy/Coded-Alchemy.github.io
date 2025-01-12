e408fb28c68923e117e127a9397d3a75db62d477
Taji Abdullah <codedalchemy@gmail.com>
2025-01-12 08:07:48 -0500

    Added Header component.

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import {Routes, Route, Link} from 'react-router-dom';
 import './App.css'
+import {Header} from "../components/header.tsx";
 
 const Home: React.FC = () => <h1>Home Page</h1>;
 
@@ -10,13 +11,16 @@ function App() {
 
     return (
         <div className="flex items-center justify-center pt-16 pb-4">
-            <nav>
-                <Link to="/">Home</Link> | <Link to="/about">About</Link>
-            </nav>
-            <Routes>
-                <Route path="/" element={<Home/>}/>
-                <Route path="/about" element={<About/>}/>
-            </Routes>
+            <div className="flex-1 flex flex-col items-center gap-16 min-h-0">
+                <Header/>
+                <nav>
+                    <Link to="/">Home</Link> | <Link to="/about">About</Link>
+                </nav>
+                <Routes>
+                    <Route path="/" element={<Home/>}/>
+                    <Route path="/about" element={<About/>}/>
+                </Routes>
+            </div>
         </div>
     )
 }
