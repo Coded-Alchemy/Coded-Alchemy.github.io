44e453eab99e89a4e8b5ab69c501dcc3c991fe13
Taji Abdullah <codedalchemy@gmail.com>
2025-01-12 07:20:19 -0500

    Working!!!

--- src/App.tsx
+++ src/App.tsx
@@ -9,17 +9,15 @@ const About: React.FC = () => <h1>About Page</h1>;
 function App() {
 
     return (
-        <>
-            <div>
-                <nav>
-                    <Link to="/">Home</Link> | <Link to="/about">About</Link>
-                </nav>
-                <Routes>
-                    <Route path="/" element={<Home/>}/>
-                    <Route path="/about" element={<About/>}/>
-                </Routes>
-            </div>
-        </>
+        <div className="flex items-center justify-center pt-16 pb-4">
+            <nav>
+                <Link to="/">Home</Link> | <Link to="/about">About</Link>
+            </nav>
+            <Routes>
+                <Route path="/" element={<Home/>}/>
+                <Route path="/about" element={<About/>}/>
+            </Routes>
+        </div>
     )
 }
 
--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/coded-alchemy.github.io/dist/',
+  base: '/../../',
   build: {
     outDir: "dist",
   },
