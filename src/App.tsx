c83f298c2faabc16db03706d38b3188ceba0e507
Taji Abdullah <codedalchemy@gmail.com>
2025-01-12 08:46:44 -0500

    Added the Footer.

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,13 @@ import React from 'react';
 import {Routes, Route, Link} from 'react-router-dom';
 import './App.css'
 import {Header} from "../components/header.tsx";
+import {Footer} from "../components/footer.tsx";
 
 const Home: React.FC = () => <h1>Home Page</h1>;
 
 const About: React.FC = () => <h1>About Page</h1>;
 
 function App() {
-
     return (
         <div className="flex items-center justify-center pt-16 pb-4">
             <div className="flex-1 flex flex-col items-center gap-16 min-h-0">
@@ -16,10 +16,13 @@ function App() {
                 <nav>
                     <Link to="/">Home</Link> | <Link to="/about">About</Link>
                 </nav>
-                <Routes>
-                    <Route path="/" element={<Home/>}/>
-                    <Route path="/about" element={<About/>}/>
-                </Routes>
+                <main>
+                    <Routes>
+                        <Route path="/" element={<Home/>}/>
+                        <Route path="/about" element={<About/>}/>
+                    </Routes>
+                </main>
+                <Footer/>
             </div>
         </div>
     )
