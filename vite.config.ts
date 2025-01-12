9931a24c5be54fc16265205fefad585dd6d3195a
Taji Abdullah <codedalchemy@gmail.com>
2025-01-11 23:56:51 -0500

    More prep

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/coded-alchemy.github.io/',
+  base: '/coded-alchemy.github.io/dist/',
   build: {
     outDir: "dist",
   },
