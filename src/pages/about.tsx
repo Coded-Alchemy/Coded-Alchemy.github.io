90542abe39760c693de4f637123df83c5beb3204
Taji Abdullah <codedalchemy@gmail.com>
2025-01-18 11:04:49 -0500

    About page update.

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -9,24 +9,44 @@ export function AboutPage() {
                         <div className="p-8">
                             <h1 className="text-3xl">About</h1>
                             <p className="mt-2 text-white-500 mt-8">
-                                As a Software Engineer with over a decade of experience, I leverage my extensive
-                                background in native Android development to deliver cutting-edge and user-friendly
-                                applications across diverse client domains. My proficiency spans the entire software
-                                development life cycle, encompassing planning, building, testing, deployment, and
-                                maintenance. Utilizing modern development practices and tools, I ensure the delivery of
-                                high-quality and efficient solutions.
+                                After 10 years of building cutting-edge Android applications and delivering exceptional
+                                mobile experiences, I am charting a bold new course into the field of cybersecurity.
+                                My decade of experience as a software engineer has equipped me with a strong foundation
+                                of transferable skills that empower me to protect and secure digital ecosystems in this
+                                new role.
                             </p>
 
-                            <p className="mt-2 text-white-500 mt-8">
-                                With a keen passion for continuous learning, I actively explore and experiment with
-                                emerging technologies and frameworks. This commitment to staying abreast of industry
-                                trends allows me to remain at the forefront of innovation. I possess a strong
-                                foundation in key areas such as Java and Kotlin for object-oriented programming, Git
-                                for version control, SQL for database management, XML, JSON, and Android Studio with
-                                the Android SDK for mobile application development. The Model-View-ViewModel (MVVM)
-                                architecture serves as a cornerstone in my approach to crafting robust mobile
-                                applications.
+                            <div className="md:shrink-0 flex justify-center items-center">
+                                <img
+                                    className="h-64 w-full object-cover md:h-full md:w-64"
+                                    src="/img/google-cybersecurity-certificate.png"
+                                    alt="Google Cybersecurity certificate badge"
+                                />
+                            </div>
+
+                            <h3 className="text-xl mt-8">Why Transition to Cybersecurity?</h3>
+                            <p className="text-white-500 mt-8">
+                                The rise in cyber threats has revealed the critical need for experts who can safeguard
+                                sensitive information and defend against attacks. While working in mobile development, I
+                                frequently encountered the need for secure coding practices and proactive threat
+                                mitigation. This exposure sparked my interest in cybersecurity, driving me to pursue
+                                certifications, training, and real-world projects in the field.
                             </p>
+
+                            <div className="flex justify-center items-center mt-16">
+                                <iframe
+                                    src="https://tryhackme.com/api/v2/badges/public-profile?userPublicId=3514365"
+                                    style={{
+                                        border: 'none',
+                                        width: '100%',
+                                        maxWidth: '329px',
+                                        height: '88px',
+                                        background: 'darkgrey'
+                                    }}
+                                    title="TryHackMe Profile Badge"
+                                ></iframe>
+                            </div>
+
                         </div>
                     </div>
                 </div>
