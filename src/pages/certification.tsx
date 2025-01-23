184f750a68f3c38026a1fa790778a9ba3d789c6b
Taji Abdullah <codedalchemy@gmail.com>
2025-01-23 07:50:14 -0500

    Added a cert.

--- src/pages/certification.tsx
+++ src/pages/certification.tsx
@@ -4,6 +4,7 @@ import "react-responsive-carousel/lib/styles/carousel.min.css";
 export function CertificationPage() {
     const images = [
         "/img/google_cs.png",
+        "/img/thm_pre_sec.png",
         "/img/thm_aoc.png",
         "img/critical_thinking_problem_solving.png",
         "/img/strategic_thinking.png",
@@ -17,7 +18,8 @@ export function CertificationPage() {
         <div className="max-w-[800px] w-full space-y-6 px-4">
             <div className="rounded-3xl border border-gray-200 p-6 dark:border-gray-700 space-y-4">
                 <div className="box">
-                    <Carousel useKeyboardArrows={true}>
+                    <h1 className="text-3xl">Certification</h1>
+                    <Carousel useKeyboardArrows={true} className="mt-8">
                         {images.map((URL, index) => (
                             <div className="slide">
                                 <img alt="Certification" src={URL} key={index}/>
